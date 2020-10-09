0e7150e6f91ac9a0db8acead31dd79f92b9ee180
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2020-10-09 17:30:55 +0200

    Add ESLint #13 (#14)
    * Install packages npm to ESLINT AIRBNB and created npm script 'lint'
    
    * Added configuration to '.eslintrc' for solve the problem: Parsing error: The keyword 'import' is reserved
    
    * Checking rules '.eslintrc'
    
    * Solve indentation problem
    
    * Solve indentation problem
    
    * Fixing error. SyntaxError: /home/runner/work/is-my-env-alive/is-my-env-alive/tests/lib/check.spec.js: Support for the experimental syntax 'classProperties' isn't currently enabled
    
    * Update package.json
    
    Co-authored-by: Renan Inácio <renaninacio.13@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,42 @@
 const path = require('path');
 const webpack = require('webpack');
 
-module.exports = {  
-  entry: {
-    'check': './lib/check.js',
-    'command': './lib/command.js'
-  },
-  target: 'node',
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: '[name].js',
-    libraryTarget: 'umd',
-  },
-  plugins: [
-    new webpack.BannerPlugin({
-      banner: "#!/usr/bin/env node",
-      raw: true,
-      test: 'command'
-    }),
-]
+module.exports = {
+	entry: {
+		'check': './lib/check.js',
+		'command': './lib/command.js'
+	},
+	target: 'node',
+	output: {
+		path: path.join(__dirname, 'dist'),
+		filename: '[name].js',
+		libraryTarget: 'umd',
+	},
+	plugins: [
+		new webpack.BannerPlugin({
+			banner: "#!/usr/bin/env node",
+			raw: true,
+			test: 'command'
+		}),
+		
+	],
+	module: {
+		rules: [
+			{
+				test: /\.(mjs|js|jsx)$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+				options: {
+					presets: [
+						'@babel/preset-env',
+						{
+							plugins: [
+								'@babel/plugin-proposal-class-properties'
+							]
+						}
+					]
+				},
+			}
+		],
+	}
 }
